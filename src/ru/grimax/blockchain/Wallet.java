4faf72127af03d8426bc40adb7d888bc17f24a35
max <maximus235711@rambler.ru>
2023-05-23 21:20:02 +0300

    Add class Wallet

---
+++ src/ru/grimax/blockchain/Wallet.java
@@ -0,0 +1,33 @@
+package ru.grimax.blockchain;
+
+import java.io.Serializable;
+import java.security.*;
+
+public class Wallet implements Serializable {
+    private KeyPair keyPair;
+
+    public Wallet() throws NoSuchAlgorithmException {
+        this(2048, KeyPairGenerator.getInstance("DSA"));
+    }
+
+    public Wallet(Integer keySize, KeyPairGenerator keyPairGen) {
+        keyPairGen.initialize(keySize);
+        this.keyPair = keyPairGen.generateKeyPair();
+    }
+
+    public Wallet(PublicKey publicKey, PrivateKey privateKey) {
+        this.keyPair = new KeyPair(publicKey, privateKey);
+    }
+
+    public KeyPair getKeyPair() {
+        return keyPair;
+    }
+
+    public  PublicKey getPublicKey() {
+        return  keyPair.getPublic();
+    }
+
+    public PrivateKey getPrivateKey() {
+        return  keyPair.getPrivate();
+    }
+}
